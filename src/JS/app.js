e6902163348d805f5ecaf1b8475b66bc99f5e7ca
Giordano Dìaz <diazgiordano@gmail.com>
2021-01-19 13:00:38 -0500

    Create functions

---
+++ src/JS/app.js
@@ -0,0 +1,33 @@
+let myLibrary = [];
+const form = document.querySelector('form');
+const ftitle = document.getElementsByClassName('.f-title');
+const fauthor = document.getElementsByClassName('.f-author');
+const fpages = document.getElementsByClassName('.f-pages');
+const fedition = document.getElementsByClassName('.f-edition');
+const fsubmit = document.getElementsByClassName('.f-submit');
+
+function Book(title,author,pages,edition){
+  let id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.edition = edition;
+}
+
+function showForm(){
+  form.className = 'show';
+}
+
+function showBooks(){
+  
+}
+
+function addNewBooks(){
+  if (ftitle.value === '' || fauthor.value === '' || fpages.value === '' || fedition.value === ''){
+    alert('There are data missing, please check it.')
+  } else {
+    const book = new Book(ftitle.value,fauthor.value,fpages.value,fedition.value);
+    myLibrary.push(book);
+    showBooks();
+  }
+}
