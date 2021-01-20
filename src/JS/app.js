e946ee78d7803b72284965dbb30db6c3c2f9a7b2
mke2111 <roymke134@gmail.com>
2021-01-21 00:05:08 +0300

    add read status in the book display

--- src/JS/app.js
+++ src/JS/app.js
@@ -4,20 +4,24 @@ const ftitle = document.querySelector('.f-title');
 const fauthor = document.querySelector('.f-author');
 const fpages = document.querySelector('.f-pages');
 const fedition = document.querySelector('.f-edition');
+const fread = document.querySelector('.f-read');
 const fsubmit = document.querySelector('.f-submit');
 const bookCard = document.querySelector('.book-card');
 
-function Book(title,author,pages,edition){
+function Book(title,author,pages,edition, read){
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.edition = edition;
+  this.read = read;
 }
 
-const book1 = new Book('Harry Potter','JK. Rowling','500','First Edition');
-const book2 = new Book('Apocalips','Giordano','800','Third Edition');
-myLibrary.push(book1);
-myLibrary.push(book2);
+
+
+// const book1 = new Book('Harry Potter','JK. Rowling','500','First Edition');
+// const book2 = new Book('Apocalips','Giordano','800','Third Edition');
+// myLibrary.push(book1);
+// myLibrary.push(book2);
 
 function showForm(){
   form.className = 'show';
@@ -27,7 +31,7 @@ function addNewBooks(){
   if (ftitle.value === '' || fauthor.value === '' || fpages.value === '' || fedition.value === ''){
     alert('There are data missing, please check it.')
   } else {
-    const book = new Book(ftitle.value,fauthor.value,fpages.value,fedition.value);
+    const book = new Book(ftitle.value,fauthor.value,fpages.value,fedition.value, fread.value);
     myLibrary.push(book);
     updateLocalStorage();
   }
@@ -51,6 +55,8 @@ function getLocalStorage() {
 function showBooks(){
   
   for (let i = 0; i < myLibrary.length; i++) {
+
+    let m = readContent(i);
     
     const cardTitle = document.createElement('h2');
     cardTitle.setAttribute('class', 'card-title');
@@ -68,12 +74,42 @@ function showBooks(){
     cardEdition.setAttribute('class', 'card-edition');
     cardEdition.innerText = `Edition: ${myLibrary[i].edition}`;
 
+    const cardRead = document.createElement('p');
+    cardRead.setAttribute('class', 'card-read');
+    cardRead.innerText = `Read: ${m}`;
+
+    const readButton = document.createElement('button');
+    readButton.innerHTML = 'Change Status';
+    readButton.setAttribute('onclick', `readStatus(${m})`);
+
     bookCard.appendChild(cardTitle);
     bookCard.appendChild(cardAuthor);
     bookCard.appendChild(cardPage);
     bookCard.appendChild(cardEdition);
+    bookCard.appendChild(cardRead);
+    bookCard.appendChild(readButton);
+
+    console.log(myLibrary[0].read);
   }
   
+}
+
+function readStatus(s) {
+  if (s == 'Read' ) {
+    s = 'Not Read';
+  } else {
+    s = 'Read';
+  }
+}
+
+function readContent(s) {
+  let status;
+  if (myLibrary[s].read == true) {
+    status = 'Read';
+  } else {
+    status = 'Not Read';
+  }
+  return status;
 }
 
 getLocalStorage();
