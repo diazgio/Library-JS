59e9d4f25d1e3158c3b0b3e126cc887fd100e9be
Giordano Dìaz <diazgiordano@gmail.com>
2021-01-19 19:10:30 -0500

    Move default objects to the first lines

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,10 +1,11 @@
 let myLibrary = [];
 const form = document.querySelector('form');
-const ftitle = document.getElementsByClassName('.f-title');
-const fauthor = document.getElementsByClassName('.f-author');
-const fpages = document.getElementsByClassName('.f-pages');
-const fedition = document.getElementsByClassName('.f-edition');
-const fsubmit = document.getElementsByClassName('.f-submit');
+const ftitle = document.querySelector('.f-title');
+const fauthor = document.querySelector('.f-author');
+const fpages = document.querySelector('.f-pages');
+const fedition = document.querySelector('.f-edition');
+const fsubmit = document.querySelector('.f-submit');
+const bookCard = document.querySelector('.book-card');
 
 function Book(title,author,pages,edition){
   this.title = title;
@@ -13,17 +14,50 @@ function Book(title,author,pages,edition){
   this.edition = edition;
 }
 
+const book1 = new Book('Harry Potter','JK. Rowling','500','First Edition');
+const book2 = new Book('Apocalips','Giordano','800','Third Edition');
+myLibrary.push(book1);
+myLibrary.push(book2);
+
 function showForm(){
   form.className = 'show';
 }
 
+function addNewBooks(){
+  if (ftitle.value === '' || fauthor.value === '' || fpages.value === '' || fedition.value === ''){
+    alert('There are data missing, please check it.')
+  } else {
+    const book = new Book(ftitle.value,fauthor.value,fpages.value,fedition.value);
+    updateLocalStorage(book);
+    showBooks();
+  }
+}
+
+function updateLocalStorage(book) {
+  if (book){
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    myLibrary.push(book);
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  }
+}
+
+function getLocalStorage() {
+  if (localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+  } else {
+    myLibrary = [];
+  }
+}
+
+
+
 function showBooks(){
-  const bookCard = document.querySelector('.book-card');
+  getLocalStorage();
   
   bookCard.innerHTML = '';
   
   for (let i = 0; i < myLibrary.length; i++) {
-    // this.myLibrary[i];
+    
     const cardTitle = document.createElement('h2');
     cardTitle.setAttribute('class', 'card-title');
     cardTitle.innerText = myLibrary[i].title;
@@ -44,37 +78,9 @@ function showBooks(){
     bookCard.appendChild(cardAuthor);
     bookCard.appendChild(cardPage);
     bookCard.appendChild(cardEdition);
-
-
   }
-}
-
-function addNewBooks(){
-  if (ftitle.value === '' || fauthor.value === '' || fpages.value === '' || fedition.value === ''){
-    alert('There are data missing, please check it.')
-  } else {
-    const book = new Book(ftitle.value,fauthor.value,fpages.value,fedition.value);
-    myLibrary.push(book);
-    showBooks();
-  }
-}
-
-const book1 = new Book('Harry Potter','JK. Rowling','500','First Edition');
-const book2 = new Book('Apocalips','Giordano','800','Third Edition');
-
-myLibrary.push(book1);
-myLibrary.push(book2);
-
-function updateLocalStorage() {
-  localStorage.setItem('myLibrary', JSON.stringify(this.myLibrary));
-}
-
-function getLocalStorage() {
-  if (localStorage.getItem('myLibrary')) {
-    this.myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
-  } else {
-    this.myLibrary = [];
-  }
+  
 }
 
 showBooks();
+console.log(myLibrary);
